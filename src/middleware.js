89005f6fdccc63160e8c2c794bffa08e6d53f84b
Reid <14616716+rao-zhilei@user.noreply.gitee.com>
2025-12-06 13:45:21 +0800

    feat: 微信公众号业务域名

--- src/middleware.js
+++ src/middleware.js
@@ -20,7 +20,7 @@ export function middleware(request) {
     '/images',
     '/dfd8f2ae8e6634994ec4216a51edbb12.txt',  // 微信域名验证文件
     '/Vc5OIbZbn2.txt',
-    'MP_verify_DyHloNZH8DTupLU3.txt'
+    '/MP_verify_DyHloNZH8DTupLU3.txt'
   ];
 
   // 检查是否是允许的路径
