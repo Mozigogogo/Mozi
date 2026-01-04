000f5f2f715a86c83c5d123e74decfb9acb56c47
Reid <14616716+rao-zhilei@user.noreply.gitee.com>
2026-01-04 15:57:47 +0800

    feat: 社区发现好币模块

---
+++ src/components/DiscoveryPostCard/index.jsx
@@ -0,0 +1,123 @@
+'use client';
+
+import { useTranslation } from 'react-i18next';
+import styles from './index.module.less';
+
+const CDN_ICON = 'https://image-1317406749.cos.ap-shanghai.myqcloud.com/assets/icon/community';
+const findBestCoinIcon = `${CDN_ICON}/find-best-coin.png`;
+const integralIcon = `${CDN_ICON}/integral.png`;
+const plateIcon = `${CDN_ICON}/plate.png`;
+const reasonIcon = `${CDN_ICON}/reason.png`;
+const messagesLikeActiveIcon = `${CDN_ICON}/messages-like-active.png`;
+const messagesLikeNoActivedIcon = `${CDN_ICON}/messages-like-no-actived.png`;
+const messagesShareIcon = `${CDN_ICON}/messages-share.png`;
+const messagesCommentIcon = `${CDN_ICON}/messages-comment.png`;
+
+/**
+ * 发现好币帖子卡片组件
+ * @param {Object} post - 帖子数据
+ * @param {Function} onPostClick - 点击帖子回调
+ * @param {Function} onUserClick - 点击用户回调
+ * @param {Function} onLikeClick - 点击点赞回调
+ * @param {Function} onShareClick - 点击分享回调
+ * @param {boolean} isLiked - 是否已点赞
+ * @param {Function} formatTimeAgo - 时间格式化函数
+ * @param {boolean} isPC - 是否为PC端，默认false
+ */
+export default function DiscoveryPostCard({
+  post,
+  onPostClick,
+  onUserClick,
+  onLikeClick,
+  onShareClick,
+  isLiked = false,
+  formatTimeAgo,
+  isPC = false
+}) {
+  const { t } = useTranslation();
+
+  return (
+    <div 
+      className={`${styles.discoveryCard} ${isPC ? styles.pcCard : styles.mobileCard}`} 
+      onClick={() => onPostClick?.(post.id)}
+    >
+      {/* 右上角装饰图标 */}
+      <img src={findBestCoinIcon} className={styles.findBestCoinBg} alt="" />
+      
+      {/* 用户信息 */}
+      <div className={styles.discoveryUserInfo}>
+        <img 
+          src={post.avatar || '/default-avatar.png'} 
+          alt="avatar" 
+          className={styles.discoveryAvatar}
+          onClick={(e) => {
+            e.stopPropagation();
+            onUserClick?.(post.userId);
+          }}
+        />
+        <div className={styles.discoveryUserContent}>
+          <span className={styles.discoveryNickname}>{post.username}</span>
+          <span className={styles.discoveryTime}>
+            {formatTimeAgo ? formatTimeAgo(post.createTime || post.updatedAt) : post.createTime}
+          </span>
+        </div>
+      </div>
+
+      {/* 币种信息区域 */}
+      <div className={styles.coinInfoSection}>
+        <div className={styles.coinInfoRow}>
+          <img className={styles.coinInfoIconImg} src={integralIcon} alt="" />
+          <span className={styles.coinInfoLabel}>{t('community.coinInfo.coinName')}</span>
+          <span className={styles.coinInfoValue}>
+            {post.tags && post.tags.length > 0 ? post.tags[0].name : 'N/A'}
+          </span>
+        </div>
+        
+        <div className={styles.coinInfoRow}>
+          <img className={styles.coinInfoIconImg} src={plateIcon} alt="" />
+          <span className={styles.coinInfoLabel}>{t('community.coinInfo.sector')}</span>
+          <span className={styles.coinInfoValue}>{post.sector || 'DeFi'}</span>
+        </div>
+        
+        <div className={styles.coinInfoRow}>
+          <img className={styles.coinInfoIconImg} src={reasonIcon} alt="" />
+          <span className={styles.coinInfoLabel}>{t('post.recommendReason')}：</span>
+          <span className={styles.coinInfoValue}>{post.content || ''}</span>
+        </div>
+      </div>
+
+      {/* 操作按钮 */}
+      <div className={styles.discoveryActionButtons}>
+        <button 
+          className={`${styles.discoveryActionBtn} ${styles.likeBtn} ${isLiked ? styles.liked : ''}`}
+          onClick={(e) => {
+            e.stopPropagation();
+            onLikeClick?.(post.id);
+          }}
+        >
+          <img
+            className={`${styles.discoveryActionIcon} ${isPC ? styles.pcIcon : ''}`}
+            src={isLiked ? messagesLikeActiveIcon : messagesLikeNoActivedIcon}
+            alt="like"
+          />
+          <span className={styles.actionCount}>{post.likeCount || 0}</span>
+        </button>
+        
+        <button 
+          className={`${styles.discoveryActionBtn} ${styles.shareBtn}`}
+          onClick={(e) => {
+            e.stopPropagation();
+            onShareClick?.(post);
+          }}
+        >
+          <img className={`${styles.discoveryActionIcon} ${isPC ? styles.pcIcon : ''}`} src={messagesShareIcon} alt="share" />
+        </button>
+        
+        <button className={`${styles.discoveryActionBtn} ${styles.commentBtn}`}>
+          <img className={`${styles.discoveryActionIcon} ${isPC ? styles.pcIcon : ''}`} src={messagesCommentIcon} alt="comment" />
+          <span className={styles.actionCount}>{post.commentCount || 0}</span>
+        </button>
+      </div>
+    </div>
+  );
+}
