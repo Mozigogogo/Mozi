f783ccc12635c2f79a5870d2ee117b0cafc031ce
Reid <14616716+rao-zhilei@user.noreply.gitee.com>
2026-02-12 14:50:58 +0800

    fix: 主题色埋点

--- src/app/theme/page.jsx
+++ src/app/theme/page.jsx
@@ -11,12 +11,13 @@ import styles from "./page.module.less";
 
 const THEMES = [
   { id: "default", nameKey: "theme.themes.default.name", descKey: "theme.themes.default.description", primaryColor: "#11B787", bgColor: "#EEF0F3", themeColor: 1 },
-  { id: "silver",  nameKey: "theme.themes.silver.name",  descKey: "theme.themes.silver.description",  primaryColor: "#8C8C8C",  bgColor: "#F5F5F5", themeColor: 3 },
-  { id: "blue",    nameKey: "theme.themes.blue.name",    descKey: "theme.themes.blue.description",    primaryColor: "#1890FF",  bgColor: "#E6F7FF", themeColor: 4 },
-  { id: "purple",  nameKey: "theme.themes.purple.name",  descKey: "theme.themes.purple.description",  primaryColor: "#722ED1",  bgColor: "#F9F0FF", themeColor: 5 },
-  { id: "orange",  nameKey: "theme.themes.orange.name",  descKey: "theme.themes.orange.description",  primaryColor: "#FA8C16",  bgColor: "#FFF7E6", themeColor: 2 },
-  { id: "red",     nameKey: "theme.themes.red.name",     descKey: "theme.themes.red.description",     primaryColor: "#F5222D",  bgColor: "#FFF1F0", themeColor: 6 },
-  { id: "dark",    nameKey: "theme.themes.dark.name",    descKey: "theme.themes.dark.description",    primaryColor: "#177DDC",  bgColor: "#141414", themeColor: 7 },
+  { id: "orange",  nameKey: "theme.themes.orange.name",  descKey: "theme.themes.orange.description",  primaryColor: "#FF7E09",  bgColor: "#FFF7E6", themeColor: 2 },
+  { id: "pink",    nameKey: "theme.themes.pink.name",    descKey: "theme.themes.pink.description",    primaryColor: "#FF4596",  bgColor: "#FFF0F6", themeColor: 3 },
+  { id: "purple",  nameKey: "theme.themes.purple.name",  descKey: "theme.themes.purple.description",  primaryColor: "#B134FF",  bgColor: "#F9F0FF", themeColor: 5 },
+  { id: "red",     nameKey: "theme.themes.red.name",     descKey: "theme.themes.red.description",     primaryColor: "#F43138",  bgColor: "#FFF1F0", themeColor: 6 },
+  { id: "black",   nameKey: "theme.themes.black.name",   descKey: "theme.themes.black.description",   primaryColor: "#161A1E",  bgColor: "#141414", themeColor: 7 },
+  { id: "blue",    nameKey: "theme.themes.blue.name",    descKey: "theme.themes.blue.description",    primaryColor: "#13399E",  bgColor: "#E6F7FF", themeColor: 4 },
+  { id: "yellow",  nameKey: "theme.themes.yellow.name",  descKey: "theme.themes.yellow.description",  primaryColor: "#E5C100",  bgColor: "#FFFFF0", themeColor: 8 },
 ];
 
 export default function ThemeCenterPage() {
